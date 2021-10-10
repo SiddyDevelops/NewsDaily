2a7b7f3b5cf334257c9c5d03b4e66acf4d74c317
hsteinmueller <harald.steinmueller@gmail.com>
2021-10-10 19:33:06 +0200

    refactor fragments with base class

--- Source Code/app/src/main/java/com/siddydevelops/newsdaily/fragments/EntertainmentFragment.java
+++ Source Code/app/src/main/java/com/siddydevelops/newsdaily/fragments/EntertainmentFragment.java
@@ -1,71 +1,11 @@
 package com.siddydevelops.newsdaily.fragments;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.siddydevelops.newsdaily.Adapter;
-import com.siddydevelops.newsdaily.ApiUtilities;
-import com.siddydevelops.newsdaily.model.Article;
 import com.siddydevelops.newsdaily.R;
-import com.siddydevelops.newsdaily.model.News;
 
-import java.util.ArrayList;
+public class EntertainmentFragment extends BaseFragment {
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-public class EntertainmentFragment extends Fragment {
-
-    String apiKEY = "20f26aa9d4274493bbbc92962ed20579";
-    ArrayList<Article> articles;
-    Adapter adapter;
-    String country = "in";
-    private RecyclerView recyclerViewEnt;
-    private String category = "entertainment";
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.entertainment_fragment,null);
-
-        recyclerViewEnt = view.findViewById(R.id.recyclerViewENTERTAINMENT);
-        articles = new ArrayList<>();
-        recyclerViewEnt.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapter = new Adapter(getContext(), articles);
-        recyclerViewEnt.setAdapter(adapter);
-
-        findNews();
-
-        return view;
+    public EntertainmentFragment() {
+        super("entertainment", R.layout.entertainment_fragment, R.id.recyclerViewENTERTAINMENT);
     }
 
-    private void findNews()
-    {
-
-        ApiUtilities.getApiInterface().getCategoryNews(country,category,100,apiKEY).enqueue(new Callback<News>() {
-            @Override
-            public void onResponse(Call<News> call, Response<News> response) {
-                if(response.isSuccessful())
-                {
-                    articles.addAll(response.body().getArticles());
-                    adapter.notifyDataSetChanged();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<News> call, Throwable t) {
-
-            }
-        });
-
-    }
 }
--- Source Code/app/src/main/java/com/siddydevelops/newsdaily/fragments/SportsFragment.java
+++ Source Code/app/src/main/java/com/siddydevelops/newsdaily/fragments/SportsFragment.java
@@ -1,71 +1,11 @@
 package com.siddydevelops.newsdaily.fragments;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.siddydevelops.newsdaily.Adapter;
-import com.siddydevelops.newsdaily.ApiUtilities;
-import com.siddydevelops.newsdaily.model.Article;
 import com.siddydevelops.newsdaily.R;
-import com.siddydevelops.newsdaily.model.News;
 
-import java.util.ArrayList;
+public class SportsFragment extends BaseFragment {
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-public class SportsFragment extends Fragment {
-
-    String apiKEY = "20f26aa9d4274493bbbc92962ed20579";
-    ArrayList<Article> articles;
-    Adapter adapter;
-    String country = "in";
-    private RecyclerView recyclerViewSports;
-    private String category = "sports";
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.sports_fragment,null);
-
-        recyclerViewSports = view.findViewById(R.id.recyclerViewSPORTS);
-        articles = new ArrayList<>();
-        recyclerViewSports.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapter = new Adapter(getContext(), articles);
-        recyclerViewSports.setAdapter(adapter);
-
-        findNews();
-
-        return view;
+    public SportsFragment() {
+        super("sports", R.layout.sports_fragment, R.id.recyclerViewSPORTS);
     }
 
-    private void findNews()
-    {
-
-        ApiUtilities.getApiInterface().getCategoryNews(country,category,100,apiKEY).enqueue(new Callback<News>() {
-            @Override
-            public void onResponse(Call<News> call, Response<News> response) {
-                if(response.isSuccessful())
-                {
-                    articles.addAll(response.body().getArticles());
-                    adapter.notifyDataSetChanged();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<News> call, Throwable t) {
-
-            }
-        });
-
-    }
 }
